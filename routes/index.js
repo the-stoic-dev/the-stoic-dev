fff99eb9a3cfccd98430953ebadec8176b1a82ac
Jeremy Greer <jeremy.greer@engageonq.com>
2018-07-13 13:50:37 -0400

    Stub GET response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Stoic Dev' });
-});
+router.get('/items', (req, res) => {
+  res.json({
+    status: 'success',
+    items: [
+      {
+        _id: 'a',
+        content: 'hello',
+      },
+      {
+        _id: 'b',
+        content: 'world',
+      },
+      ],
+  })
+})
 
 module.exports = router;
