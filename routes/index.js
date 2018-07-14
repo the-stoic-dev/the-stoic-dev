1aeea10a4bb70c5e2d7b066c66d1a6f7eaad3672
Jeremy Greer <the.stoic.dev@gmail.com>
2018-07-13 20:54:34 -0400

    Create POST items route

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const items = [
+  {
+    _id: 'a',
+    content: 'hello',
+  },
+  {
+    _id: 'b',
+    content: 'world',
+  },
+]
+
 router.get('/items', (req, res) => {
   res.json({
     status: 'success',
-    items: [
-      {
-        _id: 'a',
-        content: 'hello',
-      },
-      {
-        _id: 'b',
-        content: 'world',
-      },
-      ],
+    items,
   })
+})
+
+router.post('/items', (req, res) => {
+  console.log(req)
+  res.end()
 })
 
 module.exports = router;
