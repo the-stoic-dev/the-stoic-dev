9881fd767987339f9f3f5af37fb498c0f68e9cfd
Jeremy Greer <jeremy.greer@engageonq.com>
2018-07-16 14:33:55 -0400

    Get endpoint tests to run

--- routes/index.test.js
+++ routes/index.test.js
@@ -5,16 +5,26 @@ describe('routes/index', () => {
   describe('/items', () => {
     describe('POSTing new items', () => {
       it('should return the new item', () => {
+        const someCoolItem = { content: "blah blah" }
         return request(app)
           .post('/items')
+          .send(someCoolItem)
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .then(response => {
-            console.log('body:', response.body)
-            expect(response.body._id).toBeTruthy()
-            // expect(response.body._id).toBeTruthy()
-            // expect(response.body._id).not.toBeUndefined()
+            // testing response form
+            expect(response.body).toMatchObject({
+              status: 'success',
+              item: {},
+            })
+
+            const { item } = response.body
+            // an id was generated
+            expect(item._id).toBeTruthy()
+
+            // comparing values we sent with what was saved
+            expect(item).toMatchObject(someCoolItem)
           })
       })
     })
