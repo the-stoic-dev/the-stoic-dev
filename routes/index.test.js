d3e495d19c13512cffa808d0c1664c7377808748
Jeremy Greer <jeremy.greer@engageonq.com>
2018-07-16 14:55:03 -0400

    Test POST/GET together

--- routes/index.test.js
+++ routes/index.test.js
@@ -3,53 +3,46 @@ const request = require('supertest')
 
 describe('routes/index', () => {
   describe('/items', () => {
-    describe('POSTing new items', () => {
-      it('should return the new item', () => {
-        const someCoolItem = { content: "blah blah" }
-        return request(app)
-          .post('/items')
-          .send(someCoolItem)
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then(response => {
-            // testing response form
-            expect(response.body).toMatchObject({
-              status: 'success',
-              item: {},
-            })
-
-            const { item } = response.body
-            // an id was generated
-            expect(item._id).toBeTruthy()
-
-            // comparing values we sent with what was saved
-            expect(item).toMatchObject(someCoolItem)
+    it('should save the item and be saved', () => {
+      const someCoolItem = { content: "blah blah" }
+      // Save a new item.
+      return request(app)
+        .post('/items')
+        .send(someCoolItem)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          // testing response form
+          expect(response.body).toMatchObject({
+            status: 'success',
+            item: {},
           })
-      })
-    })
 
-    describe('GETing items', () => {
-      it('should return a list of items', () => {
-        return request(app)
-          .get('/items')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then(response => {
-            // testing response form
-            expect(response.body).toMatchObject({
-              status: 'success',
+          const { item } = response.body
+          // comparing values we sent with what was saved
+          expect(item).toMatchObject(someCoolItem)
+          return item
+        })
+        //  Now we test that we can GET the saved item.
+        .then((item) => {
+          return request(app)
+            .get('/items')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+              // testing response form
+              expect(response.body).toMatchObject({
+                status: 'success',
+              })
+
+              const { items } = response.body
+              expect(items).toBeTruthy()
+              const savedItem = items.find(x => x._id === item._id)
+              expect(savedItem).toMatchObject(item)
             })
-
-            const { items } = response.body
-            expect(items).toBeTruthy()
-            return
-
-            // comparing values we sent with what was saved
-            expect(item).toMatchObject(someCoolItem)
-          })
-      })
+        })
     })
   })
 })
