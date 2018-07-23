d7d34081b712e3d5eca979945a5e2388e2736a22
Jeremy Greer <the-stoic-dev@gmail.com>
2018-07-23 09:32:16 -0400

    WIP - Adding DELETE, interrupted video!

--- LOG.md
+++ LOG.md
@@ -1,8 +1,13 @@
 # Work Log
 
 ## Will Do
+* Look at Postman for API poking.
+* Add support to DELETE an item.
+
+## Did
 * Use separate db for test mode
 * Discuss Jest console - https://github.com/facebook/jest/issues/3853
+* Discovered secret env var being set by Jest.
 
 ## Did
 * Combined POST and GET tests into one
--- routes/index.test.js
+++ routes/index.test.js
@@ -44,5 +44,47 @@ describe('routes/index', () => {
             })
         })
     })
+
+  fit('should save the item and delete it', () => {
+      const someCoolItem = { content: "delete me, please" }
+      // Save a new item.
+      return request(app)
+        .post('/items')
+        .send(someCoolItem)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          // testing response form
+          expect(response.body).toMatchObject({
+            status: 'success',
+            item: {},
+          })
+
+          const { item } = response.body
+          // comparing values we sent with what was saved
+          expect(item).toMatchObject(someCoolItem)
+          return item
+        })
+        //  Now we test that we can GET the saved item.
+        .then((item) => {
+          return request(app)
+            .get('/items')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+              // testing response form
+              expect(response.body).toMatchObject({
+                status: 'success',
+              })
+
+              const { items } = response.body
+              expect(items).toBeTruthy()
+              const savedItem = items.find(x => x._id === item._id)
+              expect(savedItem).toMatchObject(item)
+            })
+        })
+    })
   })
 })
